8e9021ce44f1d314381f7919406e23e6f4edf542
BJudge <bartjudge@gmail.com>
2022-04-17 16:42:46 +0100

    rendered basic data on index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import './styles.scss';
+import "./styles.scss";
 
-fetch('/api/properties?location=brighton')
-  .then(response => response.json())
+// fetch("/api/properties?location=brighton")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log(json.result.properties.elements);
+//     document.querySelector("#app").innerHTML =
+//       "<p>Check the JS console for some data...</p>";
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     document.querySelector("#app").innerHTML =
+//       "<p>Something went wrong. Check the console for details.</p>";
+//   });
+
+function _renderProperty(property) {
+  return `
+                  <div class="row">
+                  <div class="col">
+                      Property Image
+                    </div>
+                    <div class="col">
+                      ${property.bedrooms}
+                    </div>
+                    <div class="col">
+                      ${property.bathrooms}
+                    </div>
+                    <div class="col">
+                      £${property.price_value}
+                    </div>
+                  </div>
+                `;
+}
+
+fetch("/api/properties?location=brighton")
+  .then((response) => response.json())
   .then((json) => {
+    const results = json.result.properties.elements;
+    let text = ` <div class="container">
+    <div class="row">
+    <div class="col">
+        PROPERTY IMAGE
+      </div>
+      <div class="col">
+        BEDROOMS
+      </div>
+      <div class="col">
+        BATHROOMS
+      </div>
+      <div class="col">
+        PRICE
+      </div>
+    </div>
+  </div>`;
+    results.forEach((element) => {
+      text = text.concat(_renderProperty(element));
+    });
+
+    document.querySelector("#arrayTest").innerHTML = text.concat("</div>");
     console.log(json.result.properties.elements);
-    document.querySelector('#app').innerHTML = '<p>Check the JS console for some data...</p>';
-  })
-  .catch((err) => {
-    console.error(err);
-    document.querySelector('#app').innerHTML = '<p>Something went wrong. Check the console for details.</p>';
   });
