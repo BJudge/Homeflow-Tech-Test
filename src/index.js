2f1dbdcb579d6ca6d03e0101a8a679ded6885987
BJudge <bartjudge@gmail.com>
2022-04-18 09:53:06 +0100

    nth child styled, favourite items moved

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import "./styles.scss";
-
-fetch("/api/properties?location=brighton")
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json.result.properties.elements);
-    document.querySelector("#app").innerHTML =
-      "<p>Check the JS console for some data...</p>";
-  })
-  .catch((err) => {
-    console.error(err);
-    document.querySelector("#app").innerHTML =
-      "<p>Something went wrong. Check the console for details.</p>";
-  });
-
 const PropertyList = document.getElementById("propertyList");
+const favouriteProperties = [];
+
+// fetch("/api/properties?location=brighton")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log(json.result.properties.elements);
+//     document.querySelector("#app").innerHTML =
+//       "<p>Check the JS console for some data...</p>";
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     document.querySelector("#app").innerHTML =
+//       "<p>Something went wrong. Check the console for details.</p>";
+//   });
 
 function _renderProperty(property) {
   const html = `
